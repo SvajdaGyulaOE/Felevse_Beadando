7d350efe209288b5ae66245e416d46c45d1efc91
SvajdaGyulaOE <Svajda_Gyula@stud.uni-obuda.hu>
2024-11-27 16:43:12 +0100

    Add files via upload

---
+++ NemGenerelSzenzorokException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feleves
+{
+    public class NemGenerelSzenzorokException : Exception
+    {
+        public NemGenerelSzenzorokException()
+        {
+            
+        }
+    }
+}
